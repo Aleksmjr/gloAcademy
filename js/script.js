9aa5d4d9fe432c117e250f9a27e99d1c47b4d5b2
«Aleksei <Aleksmjr97@gmail.com>
2025-02-18 20:02:46 +0300

    asd

--- js/script.js
+++ js/script.js
@@ -1,42 +1,49 @@
-let headers = document.getElementsByTagName('h1');
-console.log(headers[0].textContent);
+const title = document.getElementByTagName('h1')[0];
+const buttonPlus = document.querySelector('.screen-btn');
+const otherItemsPercent = document.querySelectorAll('.other-items.percent');
+const otherItemsNumber = document.querySelectorAll('.other-items.number');
 
-let buttons = document.getElementsByClassName('handler_btn');
-let calculateButton = buttons[0]; // Кнопка "Рассчитать"
-let resetButton = buttons[1]; // Кнопка "сброс"
-console.log(calculateButton.textContent);
-console.log(resetButton.textContent);
+const inputRange = document.querySelector('.rollback input');
+const inputRangeValue = document.querySelector('.rollback .range-value');
 
-let btnPlus = document.querySelector('.screen-btn');
-console.log(btnPlus);
+const startBtn = document.getElementsByClassName('handler_btn')[0];
+const resetBtn = document.getElementsByClassName('handler_btn')[1];
 
-// Элементы с классами 'other-items' и 'percent'
-let otherItemsWithPercent = document.querySelectorAll('.other-items.percent');
-
-// Элементы с классами 'other-items' и 'number'
-let otherItemsWithNumber = document.querySelectorAll('.other-items.number');
-let percentArray = Array.from(otherItemsWithPercent);
-let numberArray = Array.from(otherItemsWithNumber);
-console.log(percentArray);
-console.log(numberArray);
-
-let rangeInput = document.querySelector('.rollback input[type="range"]');
-console.log(rangeInput);
-
-let rangeValues = document.querySelector('.rollback span.range-value');
-console.log(rangeValues);
-
-let totalInputs = document.getElementsByClassName('total-input');
-
-let input1 = totalInputs[0];
-let input2 = totalInputs[1];
-let input3 = totalInputs[2];
-let input4 = totalInputs[3];
-let input5 = totalInputs[4];
-
-console.log(input1, input2, input3, input4, input5);
+const total = document.getElementsByClassName('total-input')[0];
+const totalCount = document.getElementsByClassName('total-input')[1];
+const totalCountOther = document.getElementsByClassName('total-input')[2];
+const fullTotalCount = document.getElementsByClassName('total-input')[3];
+const totalCountRollback = document.getElementsByClassName('total-input')[4];
 
 let screens = document.querySelectorAll('.screen');
-console.log(screens);
-screens = document.querySelectorAll('.new-screen'); //переопределяем?
-console.log(screens);
+
+const appData = {
+  title: '',
+  screens: [],
+  screenPrice: 0,
+  adaptive: true,
+  rollback: 10,
+  allServicePrices: 0,
+  fullPrice: 0,
+  servicePercentPrice: 0,
+  services: {},
+  init: function () {
+    appData.addTitle()
+
+    startBtn.addEventListener('click', appData.start)
+    
+  },
+  addTitle: function() {
+    document.title = title.textContent
+  }
+  start: function () {},
+  isNumber: function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  },
+  addScreens: function(){
+    
+  }
+  asking: function () {
+    appData.title = prompt('как называется ваш проект?', 'Калькулятор верстки');
+  },
+};
