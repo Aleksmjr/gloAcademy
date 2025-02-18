8216a06f58331490699faa4fbeab7eb853357d90
«Aleksei <Aleksmjr97@gmail.com>
2025-02-18 23:38:54 +0300

    white list for 12 lesson

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const title = document.getElementByTagName('h1')[0];
+const title = document.getElementsByTagName('h1')[0];
 const buttonPlus = document.querySelector('.screen-btn');
 const otherItemsPercent = document.querySelectorAll('.other-items.percent');
 const otherItemsNumber = document.querySelectorAll('.other-items.number');
@@ -23,27 +23,118 @@ const appData = {
   screenPrice: 0,
   adaptive: true,
   rollback: 10,
-  allServicePrices: 0,
+  ServicePricesPercent: 0,
+  ServicePricesNumber: 0,
   fullPrice: 0,
-  servicePercentPrice: 0,
-  services: {},
+  servicePercentPrices: 0,
+  servicesPercent: {},
+  servicesNumber: {},
   init: function () {
-    appData.addTitle()
+    appData.addTitle();
 
-    startBtn.addEventListener('click', appData.start)
-    
+    startBtn.addEventListener('click', appData.start);
+    buttonPlus.addEventListener('click', appData.addScreenBlock);
   },
-  addTitle: function() {
-    document.title = title.textContent
-  }
-  start: function () {},
-  isNumber: function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
+  addTitle: function () {
+    document.title = title.textContent;
   },
-  addScreens: function(){
-    
-  }
-  asking: function () {
-    appData.title = prompt('как называется ваш проект?', 'Калькулятор верстки');
+  // функция 1
+  start: function () {
+    appData.addScreens();
+    appData.addServices();
+    // appData.getServicePercentPrices() по дз
+    appData.addPrices();
+    appData.showResult();
+  },
+  showResult: function () {
+    total.value = appData.screenPrice;
+    totalCountOther.value =
+      appData.ServicePricesPercent + appData.ServicePricesNumber;
+    fullTotalCount.value = appData.fullPrice;
+  },
+  addServices: function () {
+    otherItemsPercent.forEach(function (item) {
+      const check = item.querySelector('input[type=checkbox]');
+      const label = item.querySelector('label');
+      const input = item.querySelector('input[type=text]');
+      if (check.checked) {
+        appData.servicesPercent[label.textContent] = +input.value;
+      }
+    });
+    otherItemsNumber.forEach(function (item) {
+      const check = item.querySelector('input[type=checkbox]');
+      const label = item.querySelector('label');
+      const input = item.querySelector('input[type=text]');
+      if (check.checked) {
+        appData.servicesNumber[label.textContent] = +input.value;
+      }
+    });
+  },
+  addScreenBlock: function () {
+    const cloneScreen = screens[0].cloneNode(true);
+
+    screens[screens.length - 1].after(cloneScreen);
+  },
+  addScreens: function () {
+    screens = document.querySelectorAll('.screen');
+
+    screens.forEach(function (screen, index) {
+      const select = screen.querySelector('select');
+      const input = screen.querySelector('input');
+      const selectName = select.options[select.selectedIndex].textContent;
+
+      appData.screens.push({
+        id: index,
+        name: selectName,
+        price: +select.value * +input.value,
+      });
+    });
+  },
+  // функция 2
+
+  // функция доп (метод )
+  addPrices: function () {
+    for (let screen of appData.screens) {
+      appData.screenPrice += +screen.price;
+    }
+    for (let key in appData.servicesNumber) {
+      appData.ServicePricesNumber += appData.servicesNumber[key];
+    }
+    for (let key in appData.servicesPercent) {
+      appData.ServicePricesPercent +=
+        appData.screenPrice * (appData.servicesPercent[key] / 100);
+    }
+    appData.fullPrice =
+      +appData.screenPrice +
+      appData.ServicePricesNumber +
+      appData.ServicePricesPercent;
+  },
+  // Функция 2
+  getRollbackMessage: function () {
+    if (appData.fullPrice >= 30000) {
+      return 'Скидка в 10%';
+    } else if (appData.fullPrice >= 15000 && appData.fullPrice < 30000) {
+      return 'Скидка в 5%';
+    } else if (appData.fullPrice < 15000) {
+      return 'Скидка не предусмотрена';
+    } else {
+      return 'Что-то пошло не так';
+    }
+  },
+
+  getServicePercentPrices: function () {
+    appData.servicePercentPrices = Math.ceil(
+      appData.fullPrice - appData.fullPrice * (appData.rollback / 100),
+    );
+  },
+  // функция 7
+  logger: function () {
+    console.log(appData.fullPrice);
+    console.log(appData.servicePercentPrices);
+    console.log(appData.screens);
+    console.log(appData.services);
   },
 };
+//
+
+appData.init();
